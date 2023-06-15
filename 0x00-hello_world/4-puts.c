07666e314048f1acb6b2444f84190213ab0e7c05
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-15 16:09:16 -0700

    s

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
+/* write program that use puts function to print line 
+ * and end with return 0 this success
+ */
 int main(void)
-{ puts("Programming is like building a multilingual puzzle");
+{ puts("Programming is like building a multilingual puzzle\n");
 	return 0 ;
 }
