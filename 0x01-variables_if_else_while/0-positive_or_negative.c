6089f216042104327bb7781ab7ce82f235219ce2
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-16 16:25:13 -0700

    s

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -11,11 +11,11 @@ int n;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-if (n % 2 == 0)
+if (n > 0)
 printf("% d is positive", n);
 else if (n == 0)
 printf("is zero");
-else if (n % 2 != 0)
+else if (n < 0)
 printf("% d is negative", n);
 return (0);
 }
