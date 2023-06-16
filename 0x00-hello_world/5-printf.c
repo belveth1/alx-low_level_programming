c7681a0f8cb3c1085e778a6478823d8bbb53ef7e
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-15 17:15:51 -0700

    s

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include <stdio.>
+/**
+ * main - A program prints line by using (printf) function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
