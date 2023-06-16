14a8494b798c66dd8202af8bf29a45d736d14c6e
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-16 15:58:35 -0700

    s

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - this function print rondom numbers and check postive or negatve
+ * Return: 0 is success
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n % 2 == 0)
+		printf(" % d is positive", n);
+	else
+		printf(" % d is negative", n);
+	return (0);
+}
