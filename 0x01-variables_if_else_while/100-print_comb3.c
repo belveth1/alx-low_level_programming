b5c3d3a7ac780550409b0eca30f9b349e3c611c9
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-17 13:11:46 -0700

    s

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ *main - this print out number with spetial (,) with putchar function
+ *Return:0 is success
+ */
+int main(void)
+{
+int a, b;
+for (a = '0' ; a <= '9 '; a++)
+{
+for (b = a + 1; b <= '9'; b++)
+{
+if (a != b)
+putchar(an);
+putchar(b);
+if (a != '8')
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+}
