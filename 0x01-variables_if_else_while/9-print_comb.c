4346c9aa710d012b9a818f2f7126b8d8dfd9ec53
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-17 03:14:21 -0700

    s

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ *main - this print out number with spetial (,) with putchar function
+ *Return:0 is success
+ */
+int main(void)
+{
+int n, m;
+for (n = 0 ; n <= 9; n++)
+{
+putchar(n + 48);
+if (n != 9)
+{
+putchar(',');
+}
+}
+putchar('\n');
+return (0);
+}
