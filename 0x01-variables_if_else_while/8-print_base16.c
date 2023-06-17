0492257f481b886efc30733d1b9f4cae203c7841
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-17 02:53:47 -0700

    s

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ *main - this print out hexa with putchar function
+ *Return:0 is success
+ */
+int main(void)
+{
+int hexa;
+for (hexa = 1; hexa <= 9; hexa++)
+putchar(hexa + 48);
+for (hexa = 'a'; hexa <= 'f'; hexa++)
+putchar(hexa);
+putchar('\n');
+return (0);
+}
