340d2ca1fd7079100a614c74361851ef99435ad2
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-16 18:48:09 -0700

    s

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - this print out numbers with putchar function
+ *Return:0 is success
+ */
+int main(void)
+{
+int num;
+for (num = 0 ; num < 10 ; num++)
+{
+putchar(num);
+}
+putchar("\n");
+return (0);
