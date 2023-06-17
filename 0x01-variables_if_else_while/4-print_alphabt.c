b20c976aee4ab162206eaaca94dc0d08df74d433
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-16 18:24:02 -0700

    s

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ *main - this print out alpha with putchar function
+ *Return:0 is success
+ */
+int main(void)
+{
+char alp;
+char e = "e";
+char q = "q";
+for (alp = 'a'; alp <= 'z'; alp++)
+{
+if (alp != q && alp != e)
+putchar(alp);
+}
+putchar('\n');
+return (0);
+}
