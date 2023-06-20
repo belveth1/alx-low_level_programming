6ba4491b899f67b4c6554d4ba319ec563832f5ec
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-20 13:17:34 -0700

    s

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ *int _islower -check for lower case
+ *Return: 1 for lower case 0 for other
+ */
+int _islower(int c)
+{
+if (c >= 97 && c <= 122)
+{
+return (1);
+}
+return (0);
+}
