22f6274a4cc081582eb64e1d2c543e2bb6202c42
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-20 16:43:17 -0700

    s

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,19 @@
 /**
  *print_last_digit - function that computes the absolute value number
  *@n: the number is to be checked
- *Return: n the abs value
+ *Return: last_digit of the number
  */
 int print_last_digit(int n)
 {
+int last_digit;
 if (n < 0)
 {
-n = (-1) * n % 10;
+last_digit = (-1) * n % 10;
 }
 else
 {
-n = n % 10;
+last_digit = n % 10;
 }
 _putchar(48 + n);
+return (last_digit);
 }
