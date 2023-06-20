da16f9721651d4e6f737da57fba33e1ba512b22f
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-20 14:35:50 -0700

    s

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *_isalpha - check for sign of number
+ *@n: the number is to be checked
+ *Return: 1 for + sign 0 for zero -1 for - sign
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+return (1);
+}
+if (n = 0)
+{
+return(0);
+}
+if (n < 0)
+{
+return (-1)
+}
+}
