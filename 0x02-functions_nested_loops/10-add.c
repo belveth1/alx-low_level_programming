4afda5c39baf4a2b4f25d96cba6c21b23669dbbf
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-20 17:36:47 -0700

    s

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include"main.h"
+
+/**
+ * add - function to add 2 integers numbers
+ * @x: first input
+ * @y: second input
+ * Return: sum of x and y
+*/
+int add(int x, int y)
+{
+int sum;
+sum = x + y;
+return (sum);
+}
