a9cafbcecfc7afd2730cc9f6c372a6b931dfa4a3
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 06:10:44 -0700

    s

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,9 +1,7 @@
 #include"main.h"
 
 /**
- * print_most_numbers - print 0 - 9 apart
- *                 from 2 and 4 and you can
- *                 only use _putchar twice
+ * print_most_numbers - print 0 - 9 but not 2 and 4
 */
 
 void print_most_numbers(void)
