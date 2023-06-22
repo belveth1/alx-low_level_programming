9c456d9db4002b002fedacc4f3c3c9c638e998d8
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 06:19:49 -0700

    s

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include"main.h"
+/**
+*more_numbers -Write a function that print numbers from 0 tp 14 not 10 times
+*/
+void more_numbers(void)
+{
+int i, count;
+for (count = 0; count <= 10 ; count++)
+{
+for (i = 0; i <= 14; i++)
+{
+_putchar(i + 48);
+}
+_putchar('\n');
+}
