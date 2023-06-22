bf4eed14cbdd22fefad38c4af1eae68e8d6aa80d
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 03:29:11 -0700

    s

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+/**
+*mul -Write a function that multiplies two integers.
+*@a: The variable to check
+*@b: The variable to check
+*Return: calc for multiplication
+*/
+int mul(int a, int b)
+{
+int calc;
+calc = a * b;
+return (calc);
