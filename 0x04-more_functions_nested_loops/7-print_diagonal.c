560968a3d794dc208d60a0b145aa5157f3d58691
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 12:33:50 -0700

    s

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include"main.h"
+/**
+*print_diagonal -Write a function that draws a diagonal line according to n
+*@n: length of diagonal
+*/
+void print_diagonal(int n)
+{
+int d, space;
+for (d = 1; d <= n; d++)
+{
+if (n <= 0)
+{
+break;
+}
+else
+{
+for (space = 1; space <= d; space++)
+{
+putchar(' ');
+}
+putchar(92);
+}
+putchar('\n');
+}
+putchar('\n');
+}
+}
