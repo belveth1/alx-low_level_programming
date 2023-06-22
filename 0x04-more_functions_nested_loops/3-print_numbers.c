b5d0038e2ed6c317107c596b2f2dfb3bffaa3072
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 03:40:54 -0700

    s

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,12 @@
+/**
+*print_numbers -Write a function that print numbers from 0 tp 9
+*Return: calc for multiplication
+*/
+void print_numbers(void)
+{int i
+for (i = 48; i <= 57; i++)
+{
+_putchar(i);
+_putchar('\n');
+}
+}
