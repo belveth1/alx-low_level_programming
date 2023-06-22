9ee03891d0ab040092b52b41397c46b7117f0326
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 01:49:58 -0700

    s

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ *_isupper - checks for uppercase character
+ *@c: The character to check
+ *Return:  1 for uppercase character 0 for otherwise.
+ */
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+else
+{
+return (0);
+}
+_putchar('\n');
+}
