17530c37f3cc2406c0d1cc56fc8c7f3ae30d73f0
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 14:15:00 -0700

    s

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,27 +4,20 @@
 */
 void main(void)
 {
-int i;
-for (i = 1; i <= 100; i++)
+int num;
+for (num = 1; num <= 100; ++num)
 {
-if (i % 3 == 0)
-{
+if (num % 3 == 0 && !(num % 5 == 0))
 printf("Fizz");
-}
-else if (i % 5 == 0)
-{
-printf("Fizz");
-}
-else if ((i % 3 == 0) && (i % 5 == 0))
-{
+else if (num % 5 == 0 && !(num % 3 == 0))
+printf("Buzz");
+else if (num % 3 == 0 && num % 5 == 0)
 printf("FizzBuzz");
-}
 else
-{
-printf("%d", i);
-}
-if (i != 100)
+printf("%d", num);
+if (num != 100)
 printf(" ");
+else
+printf("\n");
 }
-printf("\n ");
 }
