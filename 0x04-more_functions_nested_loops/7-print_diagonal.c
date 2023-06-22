e1185603f4f0deb2f3e2c60214c1a820cde49eb2
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-22 12:57:41 -0700

    s

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -5,18 +5,17 @@
 */
 void print_diagonal(int n)
 {
-int d, space;
+	int postn, space;
 
-if (n <= 0)
-_putchar('\n');
-else
-{
-for (d = 1; d <= n; ++d)
-{
-for (space = 1; space <= d; ++d)
-_putchar(' ');
-_putchar(92);
-_putchar('\n');
-}
-}
-}
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (postn = 1; postn <= n; ++postn)
+		{
+			for (space = 1; space <= postn; ++space)
+				_putchar(' ');
+			_putchar(92); /*is equal to '/' char*/
+			_putchar('\n');
+		}
+	}}
