6a8c3c4ac51634cf79ecab8bb98f1ad38e5d504b
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-26 11:56:56 -0700

    s

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen - Write a function that calculate the length of string.
+ *@s: char parametar
+ *Return: lenght of string
+ */
+int _strlen(char *s)
+{
+int n;
+for (n = 0; s != '\0')
+n++;
+return (n);
+}
