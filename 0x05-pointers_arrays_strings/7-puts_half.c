175ff89aa900d04abee5741cf988c073457c0cc1
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-26 16:19:53 -0700

    s

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,22 @@
+#include"main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: string parameter input
+*/
+void puts_half(char *str)
+{
+int i, l;
+for (i = 0; str[i] != '\0'; i++)
+;
+if (i % 2 == 0)
+{
+for (l = i / 2; str[l] != '\0'; l--)
+_putchar(str[l]);
+}
+else
+{
+for (l = (l - 1) / 2; str[l] != '\0'; l--)
+_putchar(str[l]);
+}
+_putchar('\n');
+}
