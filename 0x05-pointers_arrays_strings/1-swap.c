f2017af1294525f0ed02220238a04de0b169d4a2
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-26 06:49:49 -0700

    s

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+/**
+ *swap_int - Write a function that swaps the values of two integers.
+ *@b: int parametar
+ *@a: int parametar
+ */
+void swap_int(int *a, int *b)
+{
+int c;
+*c = *a;
+*a = *b;
+*b = *c;
+}
