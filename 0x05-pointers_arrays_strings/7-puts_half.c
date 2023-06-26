c1f3ceb57f73a62962c0f228c0fdc082517a6aef
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-26 16:28:46 -0700

    s

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -10,12 +10,12 @@ for (i = 0; str[i] != '\0'; i++)
 ;
 if (i % 2 == 0)
 {
-for (l = i / 2; str[l] != '\0'; l--)
+for (l = i / 2; str[l] != '\0'; l++)
 _putchar(str[l]);
 }
 else
 {
-for (l = ((i - 1) / 2) + 1; str[l] != '\0'; l--)
+for (l = ((i - 1) / 2) + 1; str[l] != '\0'; l++)
 _putchar(str[l]);
 }
 _putchar('\n');
