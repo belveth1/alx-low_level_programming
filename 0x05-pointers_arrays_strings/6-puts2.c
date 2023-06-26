41b10ed48d8d8e14f200ccd91ba8ca2f463c3283
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-26 14:06:30 -0700

    s

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,6 +9,12 @@ void puts2(char *str)
 int i;
 for (i = 0; str[i] != '\0'; ++i)
 ;
-for (--i; (i % 2 == 0) || (i == 0); i--)
+for (--i; i >= 0; i--)
+{
+if (i % 2 == 0)
+{
 _putchar(str[i]);
+}
+}
 _putchar('\n');
+}
