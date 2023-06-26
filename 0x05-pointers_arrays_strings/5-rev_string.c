eb7425c42b73c151ada9a630dab1cb4b2d3e415f
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-26 12:30:25 -0700

    s

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * rev_string- prints a string in reverse
+ * @s: string parameter input
+ * Return: Nothing
+*/
+void rev_string(char *s)
+{
+int i;
+for (i = 0; s[i] != '\0'; i++)
+;
+for (--i; i >= 0; --i)
+_putchar(s[i]);
+_putchar('\n');
+}
