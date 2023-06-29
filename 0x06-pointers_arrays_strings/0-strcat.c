3ec4f4c29136d80e4894c1866949c53c6b96ff81
sherif mostafa <sherifmostafa765@gmail.com>
2023-06-29 13:42:30 -0700

    s

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,16 @@
+/**
+ * _strcat - function that concatenates two strings
+ * @dest: pointer to destnation input
+ * @src: pointer to source input
+ *
+ * Return: pointer to resulting string @dest
+*/
+char *_strcat(char *dest, char *src);
+{
+int i, c;
+for (i = 0; dest[i] != '\0'; i++)
+;
+for (c = 0; src[c]; c++)
+dest[i++] = src[c];
+return (dest);
+}
