39329433acc1af346696127b0445b47c4096f4ab
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-05 15:42:48 -0700

    s

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * _puts_recursion - a function that prints a new line
+ * @s: the string
+ * Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+if (*s != '\0')
+{
+printf("%c", *s);
+_puts_recursion(s++);
+}
+else
+printf("\n");
+}
