a52623395919d73370fd159c15aa3396b53999eb
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-06 14:05:02 -0700

    s

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,38 +1,33 @@
+#include <stdio.h>
 int helper(int n, int i);
 /**
   * is_prime_number - check if the number prime or not
-  * @n : input number
-  * helper - helper function
   * @n : input number
   * Return: if the number is prime 1 if not 0
   */
 int is_prime_number(int n)
 {
-if (n <=  1)
+int is_prime_number(int n)
+{
+if (n <= 1)
 return (0);
-else if (n == 2)
+if (n == 2 || n == 3)
 return (1);
-else if (n % 2)
+if (n % 2 == 0 || n % 3 == 0)
 return (0);
-else
-return (helper(n, n / 2));
+return (helper(n, 5));
 }
+/**
+  * helper - helper function to search the number
+  * @n : input number
+  * @i : number 5
+  * Return: if the number is prime 1 if not 0
+  */
 int helper(int n, int i)
 {
-if (i == 1)
-{
+if (i * i > n)
 return (1);
-}
-else
-{
-if (n % i == 0)
-{
+if (n % i == 0 || n % (i + 2) == 0)
 return (0);
-}
-else
-{
-i--;
-return (helper(n, i));
-}
-}
+return (helper(n, i + 4));
 }
