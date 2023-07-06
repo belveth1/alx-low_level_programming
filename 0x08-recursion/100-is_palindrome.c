24457ef397ce5ffbc06265eddc535aa422f61acb
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-06 15:17:38 -0700

    s

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,19 +1,29 @@
 /**
+ *is_palindrome_helper - check if the string i spalindrome or not
+ *@s : string be checked
+ *@i : string be checked
+ *@j : string be checked
+ *Return: 1 if it is or 0 if not
+ */
+int is_palindrome_helper(char *s, int i, int j)
+{
+if (i >= j)
+{
+return (1);
+}
+if (s[i] == s[j])
+{
+return (is_palindrome_helper(s, i + 1, j - 1));
+}
+return (0);
+}
+/**
  *is_palindrome - check if the string i spalindrome or not
  *@s : string be checked
  *Return: 1 if it is or 0 if not
  */
 int is_palindrome(char *s)
 {
-int i, j;
-for (i = 0; s[i] != '\0'; i++)
-;
-i--;
-for (j = 0; s[j] != '\0'; j++)
-{
-if (s[i] != s[j])
-return (0);
-i--;
-}
-return (1);
+int len = strlen(s);
+return (is_palindrome_helper(s, 0, len - 1));
 }
