71235052eb8751cd0c1997e44402499494c860b3
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-08 02:43:11 -0700

    s

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - a program that multiplies two arguments passed into it
+ * @argc: holds the number of arguments passed
+ * @argv: array pointer that holds the arguments passed
+ * Return: Always 0 (Success)
+*/
+int main (int argc, char *argv[])
+{
+int i;
+if (argc != 3)
+{
+printf("error\n");
+return (1);
+}
+else
+{
+i = (atoi(argv[1]) * atoi(argv[2]));
+printf("%d\n", i);
+return (0);
+}
+}
