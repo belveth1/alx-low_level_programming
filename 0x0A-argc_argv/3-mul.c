dc9eb8935248d145af5a7290825fc3ee5a89ddbd
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-08 02:44:24 -0700

    s

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -7,7 +7,7 @@
  * @argv: array pointer that holds the arguments passed
  * Return: Always 0 (Success)
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
 if (argc != 3)
