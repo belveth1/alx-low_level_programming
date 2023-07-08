16650e39d61410b54e1c0197625458d5efb48476
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-08 01:48:20 -0700

    s

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,8 @@
  * @argv: array pointer that holds the arguments passed
  * Return: Always 0 (Success)
 */
-int main(int argc, char *argv[])
+int main(int __attribute__((unused))argc, char *argv[])
 {
-(void) argc;
 printf("%S\n", argv[0]);
 return (0);
 }
