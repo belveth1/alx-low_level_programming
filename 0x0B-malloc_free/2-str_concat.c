4eaa7f042d0c5e32b7721d4d7543756121a4a7e7
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-11 16:25:08 -0700

    s

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -8,18 +8,18 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-int x, y, i, j;
+int a = 0, b = 0;
+int i, j;
 char *s;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-while (s1[x] != '\0')
-x++;
-while (s2[y] != '\0')
-y++;
-/*+1 for our end of string character*/
-s = malloc((x * sizeof(char)) + ((y + 1) * sizeof(char)));
+while (s1[a] != '\0')
+a++;
+while (s2[b] != '\0')
+b++;
+s = malloc((a * sizeof(char)) + ((b + 1) * sizeof(char)));
 if (s == NULL)
 return (NULL);
 for (i = 0; s1[i] != '\0'; i++)
