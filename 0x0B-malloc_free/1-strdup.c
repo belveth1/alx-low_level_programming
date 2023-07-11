5b7fb39457b188d03642d801851231d992b14d67
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-11 15:45:01 -0700

    s

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -7,16 +7,25 @@
 */
 char *_strdup(char *str)
 {
-int i;
-char *x;
-for (i = 0; str[i] != '\0'; i++)
-;
-if (str == NULL)
-return (NULL);
-x = malloc(i * sizeof(char));
-if (x == NULL)
-return (NULL);
-for (i = 0; str[i] != '\0'; i++)
-x[i] = str[i];
-return (x);
+int i = 0, l = 0;
+	char *s;
+
+	if (str == NULL)
+		return (NULL);
+
+	/*calculate size of str*/
+	while (str[l] != '\0')
+		l++;
+
+	s = malloc((l + 1) * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		s[i] = str[i];
+		i++;
+	}
+
+	return (s);
 }
