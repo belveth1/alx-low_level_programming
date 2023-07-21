1231ea64c40fbcc39b72af94f063bd30b1636918
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-20 17:29:39 -0700

    s

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,21 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - adds all its parameters
+ *
+ * @n: start of input variables
+ *
+ * Return: the sum
+*/
+int sum_them_all(const unsigned int n, ...)
+{
+unsigned int i;
+unsigned int sum = 0;
+va_list ptr;
+if (n == 0)
+return (0);
+va_start(ptr, n);
+for (i = 0; i < n; i++)
+sum += va_arg(ptr, int);
+va_end(ptr);
+return (sum);
