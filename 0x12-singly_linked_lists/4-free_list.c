d0f3078b2d43ca0bd48fa01d1435a2f19c7ecd14
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-27 15:06:54 -0700

    s

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - free single list link
+ *
+ * @head: pointer to the first node of the list
+ *
+ * Return: nothing
+*/
+void free_list(list_t *head)
+{
+list_t *temp;
+temp = head;
+while (temp != NULL)
+{
+temp = temp->next;
+free(head);
+head = temp;
+}
+}
