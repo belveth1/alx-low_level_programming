dcba20c0e3c7f6d77a829acb5ec44ba52b964670
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-31 06:37:11 -0700

    s

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * listint_len - a function that returns the number of elements.
+ *
+ * @h: pointer to the first node
+ *
+ * Return: number of elements
+*/
+size_t listint_len(const listint_t *h)
+{
+	listint_t const *ptr;
+
+        size_t nodes = 1;
+
+        ptr = h;
+
+        if (ptr == NULL)
+                return (0);
+
+        while (ptr->next != NULL)
+        {
+                ptr = ptr->next;
+                nodes++;
+        }
+
+        return (nodes);
+}
