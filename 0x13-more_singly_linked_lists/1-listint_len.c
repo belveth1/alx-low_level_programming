7eeed4ee428331cd9dcb8dc148341692970e7f0a
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-31 06:41:03 -0700

    s

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,18 +11,18 @@ size_t listint_len(const listint_t *h)
 {
 	listint_t const *ptr;
 
-        size_t nodes = 1;
+	size_t nodes = 1;
 
-        ptr = h;
+	ptr = h;
 
-        if (ptr == NULL)
-                return (0);
+	if (ptr == NULL)
+		return (0);
 
-        while (ptr->next != NULL)
-        {
-                ptr = ptr->next;
-                nodes++;
-        }
+	while (ptr->next != NULL)
+	{
+		ptr = ptr->next;
+		nodes++;
+	}
 
-        return (nodes);
+	return (nodes);
 }
