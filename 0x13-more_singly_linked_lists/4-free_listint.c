2943a71c6cd905a9bc773ecc71e0a61ebab5d50f
sherif mostafa <sherifmostafa765@gmail.com>
2023-07-31 11:04:10 -0700

    s

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint - free list
+ *
+ * @head: pointer to the first node of the list
+ *
+ * Return: nothing
+*/
+void free_listint(listint_t *head)
+{
+	listint_t *ptr;
+
+	ptr = head;
+	while (ptr->next != NULL)
+	{
+		head = ptr->next;
+		free(ptr);
+		ptr = head;
+	}
+	free(head);
+}
