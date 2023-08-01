c2fd2dd253cca519f90f740ce043b3d0ac6d5d42
sherif mostafa <sherifmostafa765@gmail.com>
2023-08-01 14:18:16 -0700

    s

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -22,7 +22,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	ptr->next = NULL;
 	current = *head;
 
-	for (i = 0; current != NULL && i < idx - 1; i++)
+	for (i = 0; head != NULL && i < idx - 1; i++)
 	{
 		current = current->next;
 		if (current == NULL)
