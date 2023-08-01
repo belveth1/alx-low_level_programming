fc4b2cd7250a0a00299891723b1a366628b2acaa
sherif mostafa <sherifmostafa765@gmail.com>
2023-08-01 11:17:08 -0700

    s

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -17,7 +17,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	ptr = *head;
-	for (i = 0; ptr != NULL && i < index; i++)
+	for (i = 0; ptr != NULL && i < index - 1; i++)
 		ptr = ptr->next;
 
 	temp = ptr->next;
