16d705ea5e636768985006192fefa6e349fd21f5
sherif mostafa <sherifmostafa765@gmail.com>
2023-08-02 04:09:11 -0700

    s

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -13,8 +13,7 @@ size_t print_listint(const listint_t *h)
 
 	/* return 0 as no of nodes when h is null*/
 	if (h == NULL)
-		return (0);
-
+	return (0);
 	while (h->next != NULL)
 	{
 		printf("%d\n", h->n);
