bcb52f367f7fa31062bfde0ef2bb0d3150bd5f80
sherif mostafa <sherifmostafa765@gmail.com>
2023-09-04 08:53:34 -0700

    s

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -9,35 +9,20 @@
 */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
+	char *buf;
 	ssize_t fd;
-	char *buff;
-	ssize_t nbyte;
-	ssize_t wbyte;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
-		return (0);
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buff = malloc(sizeof(char) * letters);
-	if (buff == NULL)
-	{
-		free(buff);
-		return (0);
-	}
-	nbyte = read(fd, buff, letters);
-	if (nbyte == -1)
-	{
-		free(buff);
-		return (0);
-	}
-	wbyte = write(1, buff, nbyte);
-	if (wbyte == -1 || wbyte != nbyte)
-	{
-		free(buff);
-		return (0);
-	}
-	free(buff);
-	 close(fd);
-	return (wbyte);
+	buf = malloc(sizeof(char) * letters);
+	t = read(fd, buf, letters);
+	w = write(STDOUT_FILENO, buf, t);
+
+	free(buf);
+	close(fd);
+	return (w);
 }
+
