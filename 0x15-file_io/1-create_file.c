31e091155bca0d9ecf4f936f971b708865cf6ba4
sherif mostafa <sherifmostafa765@gmail.com>
2023-09-04 12:13:03 -0700

    s

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ *
+ * @filename: name of file to create
+ * @text_content: string to write to file
+ *
+ * Return: 1 on a success OR -1 on  a faliure
+*/
+int create_file(const char *filename, char *text_content)
+{
+	int fd, n;
+	int wbyte;
+
+	n = 0;
+	if (filename == NULL)
+		return (-1);
+	if (text_content != NULL)
+		while (text_content[n] != '\n')
+			n++;
+
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (fd == -1)
+		return (-1);
+	wbyte = write(fd, text_content, n);
+	if (wbyte == -1)
+		return (-1);
+	close(fd);
+	return (1);
+}
