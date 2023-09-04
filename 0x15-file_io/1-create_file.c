7e122468998c25d8311674ac7e1da762220d8a25
sherif mostafa <sherifmostafa765@gmail.com>
2023-09-04 12:18:58 -0700

    s

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,7 +17,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
-		while (text_content[n] != '\n')
+		while (text_content[n] != '\0')
 			n++;
 
 	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, S_IRUSR | S_IWUSR);
