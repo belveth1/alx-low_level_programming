d1faec046043d851be633f863db32816bad61825
sherif mostafa <sherifmostafa765@gmail.com>
2023-10-18 08:23:19 -0700

    s

--- 0x18-dynamic_libraries/main.h
+++ 0x18-dynamic_libraries/main.h
@@ -1,8 +1,5 @@
-#ifndef MAIN.H
-#define MAIN.H
-
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 int _putchar(char c);
 int _islower(int c);
@@ -25,7 +22,4 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-
-
 #endif
-
